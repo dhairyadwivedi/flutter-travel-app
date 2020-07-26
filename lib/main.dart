247204fbf8412ac402ff76e6d30edbbe0b59b243
dhairyadwivedi <dhairiya96@gmail.com>
2020-07-27 00:13:52 +0530

    Revert "Update main.dart"
    This reverts commit b7185b93c87b595181a01bc9f9c4b6209b083c99.

--- lib/main.dart
+++ lib/main.dart
@@ -21,24 +21,10 @@ class Home extends StatelessWidget {
               onPressed: () {},
             )
           ]),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: <Widget>[
-          Text('Hello, World!'),
-          RaisedButton(
-            onPressed: () {},
-            color: Colors.greenAccent,
-            child: Text('Click Me'),
-          ),
-          Container(
-            color: Colors.lightBlueAccent,
-            padding: EdgeInsets.all(20.0),
-            child: Text('Container'),
-            margin: EdgeInsets.fromLTRB(0.0, 10.0, 0.0, 0.0),
-          )
-        ],
-      ),
+      body: Center(
+          child: Image.asset(
+        ("assets/Image.jpg"),
+      )),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
         child: Text('+'),
